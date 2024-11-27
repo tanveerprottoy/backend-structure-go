03885a91e8f6e5f911e9646cdacc435cdd378cdc
tanvirprottoy <tanvir.prottoy@goldeninfotech.com.bd>
2024-11-27 13:45:35 +0600

    add sqlext new types

--- pkg/sqlext/type.go
+++ pkg/sqlext/type.go
@@ -165,3 +165,55 @@ func (j *JsonArray) Scan(val any) error {
 	}
 	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type []map[string]any", val)
 }
+
+// JsonStringArray is a type for DB json string array type
+type JsonStringArray []string
+
+// Scan implements the Scanner interface for JsonStringArray
+func (j *JsonStringArray) Scan(val any) error {
+	var jsonData []string
+	switch v := val.(type) {
+	case []byte:
+		err := json.Unmarshal(v, &jsonData)
+		if err != nil {
+			return err
+		}
+		*j = jsonData
+		return nil
+
+	case string:
+		err := json.Unmarshal([]byte(val.(string)), &jsonData)
+		if err != nil {
+			return err
+		}
+		*j = jsonData
+		return nil
+	}
+	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type []string", val)
+}
+
+// JsonIntArray is a type for DB json int array type
+type JsonIntArray []int
+
+// Scan implements the Scanner interface for JsonIntArray
+func (j *JsonIntArray) Scan(val any) error {
+	var jsonData []int
+	switch v := val.(type) {
+	case []byte:
+		err := json.Unmarshal(v, &jsonData)
+		if err != nil {
+			return err
+		}
+		*j = jsonData
+		return nil
+
+	case string:
+		err := json.Unmarshal([]byte(val.(string)), &jsonData)
+		if err != nil {
+			return err
+		}
+		*j = jsonData
+		return nil
+	}
+	return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type []int", val)
+}
